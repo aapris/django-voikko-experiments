ea54e294d68962ec370dc1dc2381720f53ce6f01
Aapo Rista <aapo.gitpub@rista.net>
2017-08-28 10:13:50 +0300

    Add local_settings.py stuff

---
+++ voiexp/local_settings_example.py
@@ -0,0 +1,10 @@
+SECRET_KEY = '.uadjgfi67&%€yuhgsdfakjhgayv&/%yugjhdfsc$y53'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['127.0.0.1', 'some.example.com', ]
+
+LANGUAGE_CODE = 'fi-fi'
+TIME_ZONE = 'Europe/Helsinki'
+
