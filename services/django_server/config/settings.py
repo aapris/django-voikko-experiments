d2915fcfe3c97f17979c8ffef41510baaa30e6f3
Aapo (porr.rista.fi) Rista <aapris@gmail.com>
2022-01-19 22:04:19 +0200

    Tweaks ports, settings and CSRF

--- services/django_server/config/settings.py
+++ services/django_server/config/settings.py
@@ -24,7 +24,8 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["voikko.rista.net", ]
+CSRF_TRUSTED_ORIGINS = ["https://*.rista.net", ]
 
 # Application definition
 
@@ -74,7 +75,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db' / 'db.sqlite3',
     }
 }
 
